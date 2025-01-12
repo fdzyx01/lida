4490d7757e1a2218198ba248fcf9f5e75235b873
fdzyx01 <1415054071@qq.com>
2025-01-12 20:13:17 +0800

    fix: token接口增加返回user_id

--- lida/web/app.py
+++ lida/web/app.py
@@ -628,7 +628,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         return {"status": False, "message": "Incorrect username or password"}
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "user_id": str(user.id)}
 
 
 @api.post("/register")
